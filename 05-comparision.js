6468cde4bcd063b624d98c587af634a7e0bc4052
Pintuizhere <pinkumar999gmail.com>
2025-10-14 13:39:55 +0530

    comparision

---
+++ 05-comparision.js
@@ -0,0 +1,26 @@
+//basic conversion
+console.log(2 > 1);
+console.log(2<1);
+console.log(2==1);
+console.log(2>=1);
+console.log(2<=1);
+
+console.log(null==0)
+console.log(null>=0)
+console.log(null<=0)
+console.log(null>0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// === strict equal to is check comparsion with datatype
+
+console.log("0"===0);
+
+
+
+
+
+
+
