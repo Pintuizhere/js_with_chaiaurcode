d6160ecb7581ef5d56bc9a5c75355c1dbf2a3a61
Pintuizhere <pinkumar999gmail.com>
2025-10-14 15:00:33 +0530

    strings

--- 06-strings.js
+++ 06-strings.js
@@ -4,7 +4,10 @@ let age = 25
 //best approach to practice 
 console.log(`my name is ${name} and my age is ${age}`);
 
-const animeName= new String("bluelock")
+const animeName = new String("bluelock")
 
+console.log(animeName[0])
+console.log(animeName.length)
 console.log(animeName.toUpperCase());
+
 
