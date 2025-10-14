8bf3f7bf5885e1666d1ac396a6e258851c2e1209
Pintuizhere <pinkumar999gmail.com>
2025-10-14 13:22:33 +0530

    operation

---
+++ 04-operation.js
@@ -0,0 +1,50 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let x=3
+let y=4
+console.log(x+y)
+/*
+console.log(2/3)
+console.log(3-2)
+console.log(5*5)
+console.log(2**3)
+console.log(6%8) //reminder modulation
+*/
+
+let str1 = "pintu"
+let str2 = " kumar"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("3" + "3")
+console.log("1" + 3 + 3)
+console.log(1 + 3 + "3")
+console.log((1 + 3) + 3 * 5)
+
+
+// console.log(+true)
+// console.log(-false)
+// console.log(+"")
+
+// postfix and prefix js mdn
+
+x = 3;
+y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
