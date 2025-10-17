af960efb8c7b197eb84246a53d6ba4e2b25d0e20
Pintuizhere <pinkumar999gmail.com>
2025-10-17 12:00:01 +0530

    dates

--- 08-dateinjs.js
+++ 08-dateinjs.js
@@ -4,3 +4,11 @@ let myDate = new Date()
 console.log(myDate)
 
 console.log(myDate.toString())
+console.log(myDate.toLocaleString());
+console.log(typeof myDate)
+
+let myCreateDate = new Date(2023, 0, 12, 5 , 6)
+
+
+console.log(myCreateDate.toDateString())
+console.log(myCreateDate.toLocaleString())
