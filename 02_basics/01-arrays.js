3e784fea292ac2994dd0185fee5df3ad973294f2
Pintu Kumar <pinkumar999@gmail.com>
2025-10-17 12:15:37 +0530

    Fix formatting in date output console log

---
+++ 02_basics/01-arrays.js
@@ -0,0 +1,17 @@
+//array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+console.log(myArr);
+console.log(myArr[2]);
+
+const newArr = new Array(10,20,30,40)
+console.log(newArr);
+
+// Array Method
+
+myArr.push(6)
+
+console.log(myArr);
+
+
