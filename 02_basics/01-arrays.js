13a9fcbc44531fbba829345a24784727dfdf1ba5
Pintuizhere <pinkumar999gmail.com>
2025-10-17 14:01:50 +0530

    array

--- 02_basics/01-arrays.js
+++ 02_basics/01-arrays.js
@@ -5,8 +5,8 @@ const myArr = [0, 1, 2, 3, 4, 5]
 console.log(myArr);
 console.log(myArr[2]);
 
-const newArr = new Array(10,20,30,40)
-console.log(newArr);
+// const newArr = new Array(10,20,30,40)
+// console.log(newArr);
 
 // Array Method
 
@@ -17,6 +17,23 @@ console.log(newArr);
 // myArr.shift()
 console.log(myArr);
 console.log(myArr.includes(10)); //find number in array as true and false
-
 console.log(myArr.indexOf(3))
+
+const newArr = myArr.join()
+console.log(newArr); //give string type
+
+//slice, splice 
+
+const myn1 = myArr.slice(1,3) //last range number not include and not any change in original array
+console.log("A", myArr);
+console.log(myn1);
+
+const myn2 = myArr.splice(1,3) //last range number include but change original array 
+console.log("B", myArr);
+console.log(myn2)
+
+
+
+
+
 
