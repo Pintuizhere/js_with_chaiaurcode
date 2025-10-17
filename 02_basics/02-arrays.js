cad5208897bfab1e6c370880eb477d69564dca23
Pintuizhere <pinkumar999gmail.com>
2025-10-17 14:34:39 +0530

    arrays

--- 02_basics/02-arrays.js
+++ 02_basics/02-arrays.js
@@ -22,5 +22,22 @@ const another_array = [1,2,[2,4],4,5,[1,3,[4,5],5],7,8]
 const realarry = another_array.flat(3) //seperate out all array in one array use flat
 console.log(realarry);
 
+console.log(Array.isArray("pintu"));
+console.log(Array.isArray([1,2,3]));
+console.log(Array.from("pintu")); //convert into array any thing by string 
+
+console.log(Array.from({name:"pintu", age: 18, number: 123994})); //interesting case for interview
+
+let num1 = 100
+let num2 = 200
+let num3 = 300
+
+console.log(Array.of(num1,num2,num3))
+
+let myname = "pintu"
+
+console.log(Array.of(myname));
 
 
+
+//study in array some important method are isArray() , from() and of()
