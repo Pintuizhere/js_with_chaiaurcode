e36af2367c04faa87a8730d215242e5efeec918f
Pintuizhere <pinkumar999gmail.com>
2025-10-17 12:14:16 +0530

    dates

--- 08-dateinjs.js
+++ 08-dateinjs.js
@@ -7,8 +7,24 @@ console.log(myDate.toString())
 console.log(myDate.toLocaleString());
 console.log(typeof myDate)
 
-let myCreateDate = new Date(2023, 0, 12, 5 , 6)
+// let myCreateDate = new Date(2023, 0, 12, 5 , 6)
+let myCreateDate = new Date("01-01-2001")
 
 
 console.log(myCreateDate.toDateString())
 console.log(myCreateDate.toLocaleString())
+console.log(myCreateDate.getTime())
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp)
+console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+
+console.log(newDate.getMonth())
+console.log(newDate.getDay())
+
+console.log(`todays date is ${newDate.toDateString()} and year is ${newDate.getFullYear()} and month is ${newDate.getMonth()} `)
+
+
