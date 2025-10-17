b38d91016c5e8bbb72afddac0de0b0be084d78c6
Pintuizhere <pinkumar999gmail.com>
2025-10-17 14:55:01 +0530

    objects

---
+++ 02_basics/03-objects.js
@@ -0,0 +1,22 @@
+//singleton - create as Object.create this is constructor
+
+//object literals
+/*
+
+const user = {}  // this object
+
+*/
+
+const user = {
+    name: "pintu",
+    age: 25,
+    location: "ranchi",
+    email: "pintu@gmail.com",
+    isLoggedIn: true,
+    lastLoginDays: ["monday","tuesday"]
+}
+
+console.log(user)
+
+
+
