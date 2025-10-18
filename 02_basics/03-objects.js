572d9a8e329e795129d226c0775507b4478bfef8
Pintuizhere <pinkumar999gmail.com>
2025-10-18 11:01:09 +0530

    objects

--- 02_basics/03-objects.js
+++ 02_basics/03-objects.js
@@ -1,22 +1,49 @@
 //singleton - create as Object.create this is constructor
 
-//object literals
-/*
 
+/*
 const user = {}  // this object
 
 */
 
+//object literals
+const mySym = Symbol("key1");
+
 const user = {
     name: "pintu",
+    "full name": "kumar",
     age: 25,
     location: "ranchi",
     email: "pintu@gmail.com",
     isLoggedIn: true,
-    lastLoginDays: ["monday","tuesday"]
-}
+    lastLoginDays: ["monday", "tuesday"],
+    [mySym]: "mykey1"
+};
 
-console.log(user)
+// console.log(user);
+// console.log(user["name"]);
+// console.log(user["full name"]);
+// console.log(user.isLoggedIn);
+// console.log(typeof user[mySym]); 
+// console.log(typeof mySym);
+
+
+// Object.freeze(user) //not change any value in object
+// user.email = "abc@gmail.com"
+// console.log(user["email"]);
+
+//function
+user.greeting = function() {
+    console.log(`hello, i am ${this.name} `);
+}
+console.log(user.greeting());
+
+
+
+
+
+
+
 
 
 
